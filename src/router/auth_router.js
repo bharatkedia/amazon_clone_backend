72ec61737cc6e37c0db8b4927c1f111c6d3c6eb3
Bharatkumar.Kedia <bharatkumar.kedia@northernarc.com>
2023-10-01 19:15:21 +0530

    implementing API to signin and signup

--- src/router/auth_router.js
+++ src/router/auth_router.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const User = require("../models/user");
 const bcryptjs = require('bcryptjs');
+const jwtToken = require("jsonwebtoken");
 
 const appRouter = express.Router();
 
 // TODO: implement authetication system.
-appRouter.post("/api/signUp", async (req, res) => {
+appRouter.post("/auth/signUp", async (req, res) => {
     try {
         console.log(req.body);
         let { name, email, password } = req.body;
@@ -28,6 +29,30 @@ appRouter.post("/api/signUp", async (req, res) => {
     } catch (e) {
         return res.status(500).json({ error: e.message });
     }
-})
+});
+
+appRouter.post("/auth/signIn",async (req, res) => {
+    try{
+        let {email, password} = req.body;
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(404).json({"message":`Username is not found : ${email}`});
+        }
+
+        let isMatch = await bcryptjs.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({"message":`Password is incorrect : ${password}`});
+        }
+
+        const token = jwtToken.sign({id: user._id, email: user.email}, "secretKey");
+
+    // "...user" will pop out all the attribute of the object (eg. {user: {a: b}} => {a:b})
+        return res.status(200).json({token, ...user._doc}); // {token: token, email:email, password:password}
+    }catch (e){
+        return res.status(500).json({error: e.message});
+    }
+});
 
 module.exports = appRouter;
